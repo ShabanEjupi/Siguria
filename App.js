8ec88ee6b7b162287af54d67f9c4695678204240
ShabanEjupi <101940223+ShabanEjupi@users.noreply.github.com>
2025-04-01 19:50:01 +0200

    Fixed app.js

--- App.js
+++ App.js
@@ -16,7 +16,15 @@ app.get('/cars', (req, res) => {
   res.sendFile(path.join(__dirname, 'cars.html'));
 });
 
+app.get('/cars.html', (req, res) => { // Shtuar rrugë alternative
+  res.sendFile(path.join(__dirname, 'cars.html'));
+});
+
 app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/about.html', (req, res) => { // Shtuar rrugë alternative
   res.sendFile(path.join(__dirname, 'about.html'));
 });
 
@@ -24,7 +32,27 @@ app.get('/contact', (req, res) => {
   res.sendFile(path.join(__dirname, 'contact.html'));
 });
 
+app.get('/contact.html', (req, res) => { // Shtuar rrugë alternative
+  res.sendFile(path.join(__dirname, 'contact.html'));
+});
+
+// Gjithashtu, shtoni një route për header.html dhe footer.html nëse i ngarkoni me AJAX
+app.get('/header.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'header.html'));
+});
+
+app.get('/footer.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'footer.html'));
+});
+
+// Raportoni 404 për rrugët e tjera
+app.use((req, res) => {
+  console.log(`404 për rrugën: ${req.path}`);
+  res.status(404).send('Faqja nuk u gjet');
+});
+
 // Startimi i serverit
 app.listen(port, () => {
   console.log(`Serveri është duke punuar në portin ${port}`);
+  console.log(`Direktoria e projektit: ${__dirname}`);
 });
